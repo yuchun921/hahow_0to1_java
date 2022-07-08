c0c36ab1ebb01451322d154b612d3d495430fb8a
Arien <xx830921xx@gmail.com>
2022-07-08 16:08:36 +0800

    update method

--- 0702_if_else/src/codegym/HomeWork.java
+++ 0702_if_else/src/codegym/HomeWork.java
@@ -3,27 +3,28 @@ package codegym;
 public class HomeWork {
 	public static void main(String[] args) {
 		// 倒轉整數值 -96463453, 43713
-		int num1 = 43713;
-		System.out.println(reverseNums(num1));
 
-		int num2 = -96463453;
+		int[] nums = { 43713, -96463453 };
 
-		if (num2 > 0) {
-			System.out.println(reverseNums(num2));
-		} else if (num2 < 0) {
-			// 將負數轉成正數
-			num2 = Math.abs(num2);
-			// 丟入reverseNums方法, 轉成reverse的char[] array
-			char[] cArray = reverseNums(num2);
-			// 將 char[] array 轉成String後, 再轉回integer
-			int number = Integer.parseInt(String.valueOf(cArray));
-			// 將 integer轉成負數
-			number = Math.negateExact(number);
-			// output
-			System.out.println(number);
-		} else {
-			System.out.println(0);
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] > 0) {
+				System.out.println(reverseNums(nums[i]));
+			} else if (nums[i] < 0) {
+				// 將負數轉成正數
+				nums[i] = Math.abs(nums[i]);
+				// 丟入reverseNums方法, 轉成reverse的char[] array
+				char[] cArray = reverseNums(nums[i]);
+				// 將 char[] array 轉成String後, 再轉回integer
+				int number = Integer.parseInt(String.valueOf(cArray));
+				// 將 integer轉成負數
+				number = Math.negateExact(number);
+				// output
+				System.out.println(number);
+			} else {
+				System.out.println(0);
+			}
 		}
+
 	}
 
 	public static char[] reverseNums(int num) {
@@ -40,6 +41,5 @@ public class HomeWork {
 		}
 		return reverseArray;
 	}
-
 
 }
