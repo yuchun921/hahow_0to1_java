add9d9b9e3c6c1f5238e194ff7999780e976f11c
Arien <xx830921xx@gmail.com>
2022-07-14 16:35:41 +0800

    Update TestDemo.java

--- 0809_garbage_collection/src/codegym/TestDemo.java
+++ 0809_garbage_collection/src/codegym/TestDemo.java
@@ -1,20 +1,19 @@
 package codegym;
 
 public class TestDemo {
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		GC gc1 = new GC("f1");
 		GC gc2 = new GC("f2");
 		GC gc3 = new GC("f3");
-		
+
 		// 變數變成null時, 物件不再被使用
 		gc1 = null;
 		gc2 = null;
 		gc3 = null;
-		
+
 		// 不會立刻執行, 等到JVM確認沒有其他物件或是方法在使用這些物件時, 才會執行
 		System.gc();
-		
+
 		// try..catch to avoding throw exception
 		try {
 			// 讓thread在仍活著的情況下暫時休息
